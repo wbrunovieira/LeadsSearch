c34e52f37107732d43e144e0b8facf19b7a2cffb
bruno-minimac <wbrunovieira77@gmail.com>
2024-09-20 07:59:41 -0300

    field leads

--- api/db/lead.go
+++ api/db/lead.go
@@ -3,15 +3,27 @@ package db
 
 type Lead struct {
 	ID               uint    `gorm:"primaryKey"`
-	Nome             string  `gorm:"size:255"`
-	Endereco         string  `gorm:"type:text"`
-	Telefone         string  `gorm:"size:50"`
-	Site             string  `gorm:"type:text"`
-	Avaliacao        float64 `gorm:"type:numeric"`
-	Qualidade        string  `gorm:"size:50"`
-	CamposPreenchidos int    `gorm:"default:0"`
-	CriadoEm         string  `gorm:"autoCreateTime"`
+	Name             string  `gorm:"size:255"`
+	Address          string  `gorm:"type:text"`
+	City          string  `gorm:"type:text"`
+	State          string  `gorm:"type:text"`
+	Country          string  `gorm:"type:text"`
+	Owner          string  `gorm:"type:text"`
+	Source          string  `gorm:"type:text"`
+	Phone            string  `gorm:"size:50"`
+	Whatsapp            string  `gorm:"size:50"`
+	Website          string  `gorm:"type:text"`
+	Email          	 string  `gorm:"type:text"`
+	Instagram          	 string  `gorm:"type:text"`
+	Facebook          	 string  `gorm:"type:text"`
+	TikTok          	 string  `gorm:"type:text"`
+	CNPJ          	 string  `gorm:"type:text"`
+	Rating           float64 `gorm:"type:numeric"`
+	Quality          string  `gorm:"size:50"`
+	FieldsFilled     int     `gorm:"default:0"`
+	CreatedAt        string  `gorm:"autoCreateTime"`
 }
+
 
 
 func CreateLead(lead *Lead) error {
