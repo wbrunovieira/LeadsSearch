6dc72e5670dc08826670e95b57c63e2c1e31253d
bruno-minimac <wbrunovieira77@gmail.com>
2024-09-24 20:12:54 -0300

    changed scrapping to selenium

--- scrapper/main.py
+++ scrapper/main.py
@@ -2,13 +2,14 @@ import os
 import time
 import json
 import random
-from urllib.parse import quote
 # pylint: disable=E0401
 import pika
-from requests_html import HTMLSession
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
 from bs4 import BeautifulSoup
-import cloudscraper
-import requests
+from urllib.parse import quote
 
 def setup_rabbitmq():
     """Configura a conexão com o RabbitMQ."""
@@ -29,6 +30,18 @@ def setup_channel(connection):
     channel.queue_bind(exchange=exchange_name, queue=queue_name)
     return channel
 
+def setup_selenium():
+    """Configura o Selenium WebDriver."""
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--remote-debugging-port=9222')
+    
+    driver = webdriver.Chrome(options=options)
+    return driver
+
 def parse_search_results(html_content, searched_city):
     """Analisa os resultados de busca e filtra por cidade."""
     soup = BeautifulSoup(html_content, 'html.parser')
@@ -47,34 +60,33 @@ def parse_search_results(html_content, searched_city):
                     results.append({'name': company_name, 'cnpj': cnpj})
     return results
 
-def fetch_url_with_proxy(url, headers, proxies_list):
-    """Busca URL usando proxies e retorna a resposta."""
-    max_retries = 5
-    for _ in range(max_retries):
-        proxy = random.choice(proxies_list)
-        try:
-            scraper = cloudscraper.create_scraper()
-            response = scraper.get(url, headers=headers, proxies=proxy, verify=False)
-            if response.status_code == 200:
-                return response
-            else:
-                print(f"Erro {response.status_code}, tentando outro proxy...")
-        except requests.exceptions.RequestException as e:
-            print(f"Erro ao conectar com o proxy {proxy}, tentando outro proxy... {e}")
-    raise Exception("Todas as tentativas de proxy falharam")
+def fetch_url_with_selenium(driver, url):
+    """Busca URL usando Selenium WebDriver e retorna o conteúdo HTML."""
+    driver.get(url)
+    time.sleep(random.uniform(1, 2))  # Aguarda o carregamento da página
+    return driver.page_source
 
-def parse_company_details(html_content):
-    """Analisa detalhes da empresa a partir do HTML."""
-    soup = BeautifulSoup(html_content, 'html.parser')
+
+
+def parse_company_details(driver):
+    """Extrai detalhes da empresa diretamente com Selenium."""
     company_info = {}
-    name_element = soup.find('h1', class_='post-title empresa-title')
-    if name_element:
+    
+    try:
+        # Extrair o nome completo da empresa
+        name_element = driver.find_element(By.CSS_SELECTOR, 'h1.post-title.empresa-title')
         company_info['full_name'] = name_element.text.strip()
-    cnpj_element = soup.find('span', text='CNPJ:')
-    if cnpj_element:
-        cnpj_value = cnpj_element.find_next('b').text.strip()
+
+        # Extrair o CNPJ
+        cnpj_element = driver.find_element(By.XPATH, "//span[contains(text(), 'CNPJ:')]")
+        cnpj_value = cnpj_element.find_element(By.XPATH, "following-sibling::b").text.strip()
         company_info['cnpj'] = cnpj_value
+
+    except Exception as e:
+        print(f"Erro ao extrair detalhes da empresa: {e}")
+
     return company_info
+
 
 def callback(ch, method, properties, body):
     """Processa mensagens da fila RabbitMQ e realiza buscas."""
@@ -87,67 +99,28 @@ def callback(ch, method, properties, body):
         if isinstance(name, str) and isinstance(city, str) and name.strip() and city.strip():
             print("Name para busca", name)
             print("City para busca", city)
-            time.sleep(random.uniform(1, 2))
-            headers = {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
-                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-                'Accept-Encoding': 'gzip, deflate, br',
-                'Accept-Language': 'en-US,en;q=0.9',
-                'Connection': 'keep-alive',
-                'Upgrade-Insecure-Requests': '1',
-                'Referer': 'https://cnpj.biz/',
-            }
-            proxies_list = [
-    {'http': 'http://64.92.82.59:8080', 'https': 'http://64.92.82.59:8080'},
-    {'http': 'http://72.10.160.171:10095', 'https': 'http://72.10.160.171:10095'},
-    {'http': 'http://72.10.164.178:1417', 'https': 'http://72.10.164.178:1417'},
-    {'http': 'http://67.43.227.228:23737', 'https': 'http://67.43.227.228:23737'},
-    {'http': 'http://160.86.242.23:8080', 'https': 'http://160.86.242.23:8080'},
-    {'http': 'http://37.187.25.85:80', 'https': 'http://37.187.25.85:80'},
-    {'http': 'http://52.67.10.183:80', 'https': 'http://52.67.10.183:80'},
-    {'http': 'http://13.38.153.36:80', 'https': 'http://13.38.153.36:80'},
-    {'http': 'http://18.228.149.161:80', 'https': 'http://18.228.149.161:80'},
-    {'http': 'http://3.212.148.199:80', 'https': 'http://3.212.148.199:80'},
-    {'http': 'http://13.37.59.99:80', 'https': 'http://13.37.59.99:80'},
-    {'http': 'http://52.196.1.182:80', 'https': 'http://52.196.1.182:80'},
-    {'http': 'http://43.202.154.212:80', 'https': 'http://43.202.154.212:80'},
-    {'http': 'http://13.208.56.180:80', 'https': 'http://13.208.56.180:80'},
-    {'http': 'http://35.72.118.126:80', 'https': 'http://35.72.118.126:80'},
-    {'http': 'http://13.37.73.214:80', 'https': 'http://13.37.73.214:80'},
-    {'http': 'http://3.141.217.225:80', 'https': 'http://3.141.217.225:80'},
-    {'http': 'http://3.124.133.93:80', 'https': 'http://3.124.133.93:80'},
-    {'http': 'http://35.76.62.196:80', 'https': 'http://35.76.62.196:80'},
-    {'http': 'http://18.228.198.164:80', 'https': 'http://18.228.198.164:80'},
-    {'http': 'http://43.201.121.81:80', 'https': 'http://43.201.121.81:80'},
-    {'http': 'http://3.127.121.101:80', 'https': 'http://3.127.121.101:80'},
-    {'http': 'http://13.37.89.201:80', 'https': 'http://13.37.89.201:80'},
-    {'http': 'http://3.71.239.218:80', 'https': 'http://3.71.239.218:80'},
-    {'http': 'http://3.126.147.182:80', 'https': 'http://3.126.147.182:80'},
-    {'http': 'http://3.127.62.252:80', 'https': 'http://3.127.62.252:80'},
-    {'http': 'http://3.122.84.99:80', 'https': 'http://3.122.84.99:80'}
-                                    ]
+            
+            # Configura o Selenium WebDriver
+            driver = setup_selenium()
 
             search_url = f"https://cnpj.biz/procura/{quote(name)}%20{quote(city)}"
             print("search_url", search_url)
 
-            response = fetch_url_with_proxy(search_url, headers, proxies_list)
+            html_content = fetch_url_with_selenium(driver, search_url)
+            search_results = parse_search_results(html_content, city)
 
-            if response.status_code == 200:
-                html_content = response.text
-                search_results = parse_search_results(html_content, city)
-                for result in search_results:
-                    cnpj = result['cnpj']
-                    company_url = f"https://cnpj.biz/{cnpj}"
-                    print("company_url", company_url)
-                    company_response = fetch_url_with_proxy(company_url, headers, proxies_list)
-                    if company_response.status_code == 200:
-                        company_soup = BeautifulSoup(company_response.text, 'html.parser')
-                        print(company_soup.prettify())
-                        print(f"Detalhes da Empresa: {name} e com CNPJ {cnpj}")
-                    else:
-                        print(f"Falha ao acessar dados da empresa com CNPJ {cnpj}")
-            else:
-                print(f"Falha ao buscar dados de {name} em {city}")
+            for result in search_results:
+                cnpj = result['cnpj']
+                company_url = f"https://cnpj.biz/{cnpj}"
+                print("company_url", company_url)
+
+                company_html_content = fetch_url_with_selenium(driver, company_url)
+                company_details = parse_company_details(company_html_content)
+                print(f"Detalhes da Empresa: {company_details}")
+            
+            # Fecha o driver após o processamento
+            driver.quit()
+
         else:
             print("Nome ou Cidade não encontrados no lead.")
     except json.JSONDecodeError as e:
