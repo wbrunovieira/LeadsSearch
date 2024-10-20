19d4d275e729c57d90758a0e94493d7737a7c2c0
bruno-minimac <wbrunovieira77@gmail.com>
2024-10-20 18:19:16 -0300

    started backend in front for logs

---
+++ frontend/backend-service/src/server.ts
@@ -0,0 +1,19 @@
+import Fastify from "fastify";
+
+const fastify = Fastify({ logger: true });
+
+fastify.get("/", async (request, reply) => {
+    return { hello: "world" };
+});
+
+const start = async () => {
+    try {
+        await fastify.listen({ port: 3333 });
+        console.log("Servidor rodando na porta 3333");
+    } catch (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+};
+
+start();
